1285b463a2bdbda49d9b36ae760b22a81d4f6e56
Alejandro Domínguez <adomu4@gmail.com>
2024-11-28 16:31:20 +0100

    Separate vim configuration

--- modules/env-dev.nix
+++ modules/env-dev.nix
@@ -10,41 +10,6 @@ params@{ pkgs, ... }:
     fzf
     git
 
-    (pkgs.vim_configurable.customize {
-      name = "vim";
-
-      vimrcConfig = {
-        customRC = ''
-          set nocompatible
-
-          set number relativenumber
-          set wrap
-          set scrolloff=8
-          set backspace=indent,eol,start
-          set mouse=a
-
-          set hidden
-          set noswapfile
-          set incsearch
-        '';
-
-        packages.myplugins.start = with pkgs.vimPlugins; [
-          vim-vinegar
-          fzf-vim
-          undotree
-          vim-dispatch
-
-          vim-fugitive
-          vim-gitgutter
-          vim-lsp
-          vim-lsp-settings
-          vim-tmux-navigator
-
-          vimtex
-          vim-pandoc
-          vim-pandoc-syntax
-        ];
-      };
-    })
+    (import ./vim.nix params)
   ];
 }
