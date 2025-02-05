7a90721c5fbefbe46096acb6910f8767dbc87f93
Alejandro Domínguez <adomu4@gmail.com>
2025-02-05 14:14:12 +0000

    Leave only distrobox as virtualisation utility

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,14 +1,5 @@
 { pkgs, ... }:
 
 {
-  virtualisation = {
-    containers.enable = true;
-    podman = {
-      enable = true;
-      dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
-    };
-  };
-
   environment.systemPackages = with pkgs; [ distrobox ];
 }
