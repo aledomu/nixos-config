751a72dce0e42c145a787f8d8a7989d9cdfe11ce
Alejandro Domínguez <adomu4@gmail.com>
2025-02-05 14:08:42 +0000

    Remove nix-on-droid setup

--- flake.nix
+++ flake.nix
@@ -3,30 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-
-    nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, nix-on-droid, ... }: {
+  outputs = { self, nixpkgs, nixos-wsl, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
       modules = [
         nixos-wsl.nixosModules.default
         ./wsl.nix
-      ];
-    };
-
-    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
-      pkgs = import nixpkgs { system = "aarch64-linux"; };
-
-      modules = [
-        ./nix-on-droid.nix
       ];
     };
   };
